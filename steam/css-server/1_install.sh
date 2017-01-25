df558aa165b77f4948363670be36f48d9aa24451
Nick Grealy <nickgrealy@gmail.com>
2017-01-25 22:52:39 +1100

    Updates.

--- steam/css-server/1_install.sh
+++ steam/css-server/1_install.sh
@@ -11,7 +11,7 @@ sudo rm -Rf /var/cache/yum/x86_64/latest
 # setup steam...
 sudo useradd -m steam
 sudo chown steam:steam /home/steam
-wget https://raw.githubusercontent.com/nickgrealy/aws-projects/master/steam/css-server/2_server.sh -P /tmp
+curl https://raw.githubusercontent.com/nickgrealy/aws-projects/master/steam/css-server/2_server.sh -o /tmp/2_server.sh
 chmod 777 /tmp/2_server.sh
 sudo su - steam -c 'export SERVER_PASSWORD=test123; /tmp/2_server.sh' &
 
