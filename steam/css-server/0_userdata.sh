84c73e5e0e329202ff6b36a43aad29eb8612ff6c
Nick Grealy <nickgrealy@gmail.com>
2017-01-26 12:05:58 +1100

    WIP

--- steam/css-server/0_userdata.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-curl https://raw.githubusercontent.com/nickgrealy/aws-projects/master/steam/css-server/1_install.sh -o /tmp/1_install.sh
-sudo chmod 777 /tmp/1_install.sh
-sudo /tmp/1_install.sh
